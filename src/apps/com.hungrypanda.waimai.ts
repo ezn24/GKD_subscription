47702b875eb9e780f53bf36d1dd0a13e54245b13
ezn24 <62716651+ezn24@users.noreply.github.com>
2026-03-15 01:43:19 +1100

    feat: 熊猫外卖-全屏广告 结算弹窗广告 (#63)
    * feat: 熊猫外卖-全屏广告-弹窗广告 结算弹窗广告
    
    * feat: 注释
    
    Updated ad group names and snapshot URLs for clarity.
    
    Signed-off-by: Ling0402 <cjy081219@Gmail.com>
    
    * perf: 增加关系限制防止潜在误触
    
    Updated matches selector for visibility and child count.
    
    Signed-off-by: Ling0402 <cjy081219@Gmail.com>
    
    ---------
    
    Signed-off-by: Ling0402 <cjy081219@Gmail.com>
    Co-authored-by: Ling0402 <cjy081219@Gmail.com>

--- src/apps/com.hungrypanda.waimai.ts
+++ src/apps/com.hungrypanda.waimai.ts
@@ -6,20 +6,23 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-弹窗广告',
+      name: '全屏广告',
       rules: [
         {
           key: 0,
           fastQuery: true,
-          activityIds:
+          activityIds: [
             'com.haya.app.pandah4a.ui.sale.home.container.HomeContainerActivity',
+            'com.haya.app.pandah4a.ui.pay.success.order.PaySuccessActivity',
+          ],
           matches: [
-            '[vid="iv_home_ad_close" || vid="iv_close"][clickable=true][visibleToUser=true]',
+            'ViewGroup[childCount=2 || childCount=3] > [vid="iv_home_ad_close" || vid="iv_close"][clickable=true][visibleToUser=true]',
           ],
           snapshotUrls: [
-            'https://i.gkd.li/i/25813448', // iv_home_ad_close
+            'https://i.gkd.li/i/25813448', // iv_home_ad_close_首页
             'https://i.gkd.li/i/25822079', // iv_home_ad_close
             'https://i.gkd.li/i/25813449', // iv_close
+            'https://i.gkd.li/i/25995791', // iv_home_ad_close_结算页
           ],
         },
       ],
