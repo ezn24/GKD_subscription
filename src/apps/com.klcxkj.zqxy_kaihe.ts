c6caaa3a44ec25ce13de8673d76454130c61ff46
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-03-14 13:20:19 +0800

    fix: 悦享校园 局部广告&全屏广告 (#61)

--- src/apps/com.klcxkj.zqxy_kaihe.ts
+++ src/apps/com.klcxkj.zqxy_kaihe.ts
@@ -8,28 +8,35 @@ export default defineGkdApp({
       key: 1,
       name: '局部广告',
       fastQuery: true,
-      activityIds: '.ui.MainUserActivity',
+      activityIds: [
+        '.ui.MainUserActivity',
+        'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
+      ],
       rules: [
         {
           key: 0,
           matches:
-            '@ImageView < FrameLayout[index=parent.childCount.minus(1)] <n FrameLayout[index=parent.childCount.minus(1)] <n LinearLayout <<4 [vid="adv_container_layout"]',
-          snapshotUrls: 'https://i.gkd.li/i/25928209',
-          exampleUrls: 'https://e.gkd.li/526e700e-018e-4642-a89e-e7936b17dd2a',
+            '@ImageView[width<63] <<(1,2) [index=parent.childCount.minus(1)] - [childCount=2] > [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/25928209', //下载应用
+            'https://i.gkd.li/i/25989673', //立即安装
+            'https://i.gkd.li/i/25989877', //立即领取
+            'https://i.gkd.li/i/25929445', //立即打开 <<2
+          ],
+          exampleUrls: 'https://e.gkd.li/22de1939-c412-40ce-a979-4aeb3f7f5923',
         },
         {
           key: 1,
           matches:
-            'LinearLayout[childCount=2] - @ImageView[clickable=true][visibleToUser=true] <2 FrameLayout[childCount=3] < [vid="adv_container_layout"]',
-          snapshotUrls: 'https://i.gkd.li/i/25928092',
-          exampleUrls: 'https://e.gkd.li/341cfc8b-b625-4ee0-9928-f4a302f491fe',
+            '@ImageView[width<63] < FrameLayout <3 FrameLayout +2 FrameLayout >2 [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
+          snapshotUrls: 'https://i.gkd.li/i/25989928', //立即下载
         },
         {
           key: 2,
           matches:
-            'ImageView < @[clickable=true] < ViewGroup - ViewGroup > [text^="立即" || text$="详情" || text^="了解" || text="去微信看看" || text$="应用" || text="进入小程序" || text="领取优惠" || text="跳转微信"]',
-          snapshotUrls: 'https://i.gkd.li/i/25929445',
-          exampleUrls: 'https://e.gkd.li/22de1939-c412-40ce-a979-4aeb3f7f5923',
+            'LinearLayout[childCount=2] - @ImageView[clickable=true][visibleToUser=true] <2 FrameLayout[childCount=3] < [vid="adv_container_layout"]',
+          snapshotUrls: 'https://i.gkd.li/i/25928092',
+          exampleUrls: 'https://e.gkd.li/341cfc8b-b625-4ee0-9928-f4a302f491fe',
         },
       ],
     },
@@ -69,6 +76,22 @@ export default defineGkdApp({
           matches: '@[clickable=true] > [vid="beizi_interstitial_ad_close_iv"]',
           snapshotUrls: 'https://i.gkd.li/i/25929116',
           exampleUrls: 'https://e.gkd.li/cca3f65f-1441-4bd6-88da-e61c325f6340',
+        },
+        {
+          key: 4, //字节sdk
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
+          matches:
+            '@ImageView[width<85] < FrameLayout <2 FrameLayout <2 FrameLayout <2 * - FrameLayout > [text*="第三方应用" || text*="扭动" || (text*="点击" && text*="上滑") || text*="省钱好物" || text*="扭一扭"]',
+          snapshotUrls: 'https://i.gkd.li/i/25989677',
+        },
+        {
+          key: 5, //字节sdk
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches:
+            '@Image[width<72] <<2 View <2 View < View <2 View <3 View <<8 FrameLayout <2 FrameLayout <<3 FrameLayout <3 FrameLayout <<2 [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/i/25991003',
         },
       ],
     },
